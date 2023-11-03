935594bf0fe20be5af4d8da3b452abed303889cf
Mishal Turkane <95625543+mishalturkane@users.noreply.github.com>
2023-11-03 20:25:50 +0530

    Add files via upload

---
+++ day17/Demo.java
@@ -0,0 +1,14 @@
+package day17;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Demo extends  Object{
+
+    public static void main(String[] args) {
+       String name="Mishal";
+       String name2="Mishal";
+       System.out.println(name2==name);
+    }
+
+}
